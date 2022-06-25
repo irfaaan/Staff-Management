acc7b883ca969e501c8e674633e893e6b1ce1d3c
irfaaan <92865038+irfaaan@users.noreply.github.com>
2022-06-25 14:17:26 +0500

    Modified

--- resources/views/partials/menu.blade.php
+++ resources/views/partials/menu.blade.php
@@ -71,18 +71,18 @@
                         </a>
                     </li>
                 @endcan
-                @can('transaction_access')
-                    <li class="nav-item">
-                        <a href="{{ route("admin.transactions.index") }}" class="nav-link {{ request()->is('admin/transactions') || request()->is('admin/transactions/*') ? 'active' : '' }}">
-                            <i class="fa-fw fas fa-credit-card">
+                {{--@can('transaction_access')--}}
+                    {{--<li class="nav-item">--}}
+                        {{--<a href="{{ route("admin.transactions.index") }}" class="nav-link {{ request()->is('admin/transactions') || request()->is('admin/transactions/*') ? 'active' : '' }}">--}}
+                            {{--<i class="fa-fw fas fa-credit-card">--}}
 
-                            </i>
-                            <p>
-                                <span>{{ trans('cruds.transaction.title') }}</span>
-                            </p>
-                        </a>
-                    </li>
-                @endcan
+                            {{--</i>--}}
+                            {{--<p>--}}
+                                {{--<span>{{ trans('cruds.transaction.title') }}</span>--}}
+                            {{--</p>--}}
+                        {{--</a>--}}
+                    {{--</li>--}}
+                {{--@endcan--}}
                 @can('client_management_setting_access')
                     <li class="nav-item has-treeview {{ request()->is('admin/currencies*') ? 'menu-open' : '' }} {{ request()->is('admin/transaction-types*') ? 'menu-open' : '' }} {{ request()->is('admin/income-sources*') ? 'menu-open' : '' }} {{ request()->is('admin/client-statuses*') ? 'menu-open' : '' }} {{ request()->is('admin/project-statuses*') ? 'menu-open' : '' }}">
                         <a class="nav-link nav-dropdown-toggle" href="#">
@@ -95,42 +95,42 @@
                             </p>
                         </a>
                         <ul class="nav nav-treeview">
-                            @can('currency_access')
-                                <li class="nav-item">
-                                    <a href="{{ route("admin.currencies.index") }}" class="nav-link {{ request()->is('admin/currencies') || request()->is('admin/currencies/*') ? 'active' : '' }}">
-                                        <i class="fa-fw fas fa-money-bill">
+                            {{--@can('currency_access')--}}
+                                {{--<li class="nav-item">--}}
+                                    {{--<a href="{{ route("admin.currencies.index") }}" class="nav-link {{ request()->is('admin/currencies') || request()->is('admin/currencies/*') ? 'active' : '' }}">--}}
+                                        {{--<i class="fa-fw fas fa-money-bill">--}}
 
-                                        </i>
-                                        <p>
-                                            <span>{{ trans('cruds.currency.title') }}</span>
-                                        </p>
-                                    </a>
-                                </li>
-                            @endcan
-                            @can('transaction_type_access')
-                                <li class="nav-item">
-                                    <a href="{{ route("admin.transaction-types.index") }}" class="nav-link {{ request()->is('admin/transaction-types') || request()->is('admin/transaction-types/*') ? 'active' : '' }}">
-                                        <i class="fa-fw fas fa-money-check">
+                                        {{--</i>--}}
+                                        {{--<p>--}}
+                                            {{--<span>{{ trans('cruds.currency.title') }}</span>--}}
+                                        {{--</p>--}}
+                                    {{--</a>--}}
+                                {{--</li>--}}
+                            {{--@endcan--}}
+                            {{--@can('transaction_type_access')--}}
+                                {{--<li class="nav-item">--}}
+                                    {{--<a href="{{ route("admin.transaction-types.index") }}" class="nav-link {{ request()->is('admin/transaction-types') || request()->is('admin/transaction-types/*') ? 'active' : '' }}">--}}
+                                        {{--<i class="fa-fw fas fa-money-check">--}}
 
-                                        </i>
-                                        <p>
-                                            <span>{{ trans('cruds.transactionType.title') }}</span>
-                                        </p>
-                                    </a>
-                                </li>
-                            @endcan
-                            @can('income_source_access')
-                                <li class="nav-item">
-                                    <a href="{{ route("admin.income-sources.index") }}" class="nav-link {{ request()->is('admin/income-sources') || request()->is('admin/income-sources/*') ? 'active' : '' }}">
-                                        <i class="fa-fw fas fa-database">
+                                        {{--</i>--}}
+                                        {{--<p>--}}
+                                            {{--<span>{{ trans('cruds.transactionType.title') }}</span>--}}
+                                        {{--</p>--}}
+                                    {{--</a>--}}
+                                {{--</li>--}}
+                            {{--@endcan--}}
+                            {{--@can('income_source_access')--}}
+                                {{--<li class="nav-item">--}}
+                                    {{--<a href="{{ route("admin.income-sources.index") }}" class="nav-link {{ request()->is('admin/income-sources') || request()->is('admin/income-sources/*') ? 'active' : '' }}">--}}
+                                        {{--<i class="fa-fw fas fa-database">--}}
 
-                                        </i>
-                                        <p>
-                                            <span>{{ trans('cruds.incomeSource.title') }}</span>
-                                        </p>
-                                    </a>
-                                </li>
-                            @endcan
+                                        {{--</i>--}}
+                                        {{--<p>--}}
+                                            {{--<span>{{ trans('cruds.incomeSource.title') }}</span>--}}
+                                        {{--</p>--}}
+                                    {{--</a>--}}
+                                {{--</li>--}}
+                            {{--@endcan--}}
                             @can('client_status_access')
                                 <li class="nav-item">
                                     <a href="{{ route("admin.client-statuses.index") }}" class="nav-link {{ request()->is('admin/client-statuses') || request()->is('admin/client-statuses/*') ? 'active' : '' }}">
